9d1e671dbd31d9e18f9dddc0bac1d199fd5aaee0
Francesc <francesc.bgr+ifrins@gmail.com>
2012-07-20 10:48:28 +0200

    Updated manual download message

--- src/com/ifrins/hipstacast/HipstacastEpisodeView.java
+++ src/com/ifrins/hipstacast/HipstacastEpisodeView.java
@@ -29,15 +29,24 @@ public class HipstacastEpisodeView extends ListActivity {
 	int episodes_count;
 
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ((Hipstacast)getApplicationContext()).trackPageView("/episodes");
-        getActionBar().setTitle(getIntent().getExtras().getString("show_title"));
-        show_id = Integer.parseInt(getIntent().getExtras().getString("show_id"));
-        Cursor p = managedQuery(Uri.parse("content://com.ifrins.hipstacast.provider.HipstacastContentProvider/podcasts/"+getIntent().getExtras().getString("show_id")+"/episodes"), 
-        						new String[] {"_id", "title", "duration", "podcast_id", "status", "position", "content_url", "content_length", "publication_date", "type"}, "podcast_id = ?", new String[] {getIntent().getExtras().getString("show_id")}, "publication_date DESC");
-        episodes_count = p.getCount();
-        setListAdapter(new EpisodeListCursorAdapter(getApplicationContext(), p));
-		
+		super.onCreate(savedInstanceState);
+		((Hipstacast) getApplicationContext()).trackPageView("/episodes");
+		getActionBar()
+				.setTitle(getIntent().getExtras().getString("show_title"));
+		show_id = Integer
+				.parseInt(getIntent().getExtras().getString("show_id"));
+		Cursor p = managedQuery(
+				Uri.parse("content://com.ifrins.hipstacast.provider.HipstacastContentProvider/podcasts/"
+						+ getIntent().getExtras().getString("show_id")
+						+ "/episodes"), new String[] { "_id", "title",
+						"duration", "podcast_id", "status", "position",
+						"content_url", "content_length", "publication_date",
+						"type" }, "podcast_id = ?", new String[] { getIntent()
+						.getExtras().getString("show_id") },
+				"publication_date DESC");
+		episodes_count = p.getCount();
+		setListAdapter(new EpisodeListCursorAdapter(getApplicationContext(), p));
+
 		final ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
 		registerForContextMenu(listView);
@@ -46,71 +55,122 @@ public class HipstacastEpisodeView extends ListActivity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					final int position, long id) {
 
-				 final Cursor c = (Cursor) getListAdapter().getItem(position);
-				 int status = c.getInt(c.getColumnIndex("status"));
-				 final int episode_id = c.getInt(c.getColumnIndex("_id"));
-				 final int podcast_id = c.getInt(c.getColumnIndex("podcast_id"));
-				 final String content_url = c.getString(c.getColumnIndex("content_url"));
-				 final String title = c.getString(c.getColumnIndex("title"));
-				 final long content_length = c.getLong(c.getColumnIndex("content_length"));
-				 final int content_type = c.getInt(c.getColumnIndex("type"));
-				 
-				 File f = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/Android/data/com.ifrins.hipstacast/files/shows/"+podcast_id+"/"+episode_id + ".mp3");
-				 File _f = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/Android/data/com.ifrins.hipstacast/files/shows/.nomedia");
-				 if (!_f.exists()) {
-					 try {
+				final Cursor c = (Cursor) getListAdapter().getItem(position);
+				int status = c.getInt(c.getColumnIndex("status"));
+				final int episode_id = c.getInt(c.getColumnIndex("_id"));
+				final int podcast_id = c.getInt(c.getColumnIndex("podcast_id"));
+				final String content_url = c.getString(c
+						.getColumnIndex("content_url"));
+				final String title = c.getString(c.getColumnIndex("title"));
+				final long content_length = c.getLong(c
+						.getColumnIndex("content_length"));
+				final int content_type = c.getInt(c.getColumnIndex("type"));
+
+				File f = new File(android.os.Environment
+						.getExternalStorageDirectory().getAbsolutePath()
+						+ "/Android/data/com.ifrins.hipstacast/files/shows/"
+						+ podcast_id + "/" + episode_id + ".mp3");
+				File _f = new File(
+						android.os.Environment.getExternalStorageDirectory()
+								.getAbsolutePath()
+								+ "/Android/data/com.ifrins.hipstacast/files/shows/.nomedia");
+				if (!_f.exists()) {
+					try {
 						_f.createNewFile();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				 }
-				 if (status == 0 && !f.exists()) {
-		            	new AlertDialog.Builder(listView.getContext())
-		                .setTitle(c.getString(c.getColumnIndex("title")))
-		                .setMessage(String.format(getString(R.string.episode_not_downloaded), (content_length/1024)/1024))
-		                .setPositiveButton(R.string.download, new DialogInterface.OnClickListener() {
-		                    public void onClick(DialogInterface dialog, int whichButton) {
-		                    	File d = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/Android/data/com.ifrins.hipstacast/files/shows/"+podcast_id);
-		                    	if(d.exists() == false) {
-		        	                d.mkdirs();
-		                    	}
-		                    	
-		                    	d = null;
-		                    	DownloadManager mgr = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-		                    	mgr.enqueue(new DownloadManager.Request(Uri.parse(content_url))
-		                    				.setTitle(title)
-		                    				.setDestinationInExternalFilesDir(getApplicationContext(), null, "shows/"+podcast_id+"/"+episode_id + ".mp3"));
-		                    				
-		                    }
-		                }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-		                    public void onClick(DialogInterface dialog, int whichButton) {
-		                        // Do nothing.
-		                    }
-		                }).show();
+				}
+				if (status == 0 && !f.exists()) {
+					String msg = null;
+					if (content_length >= 1024 * 1024) {
+						msg = String.format(
+								getString(R.string.episode_not_downloaded),
+								(content_length / 1024) / 1024);
+					} else {
+						msg = String.format(
+								getString(R.string.episode_not_downloaded),
+								"\u221E");
+					}
+					new AlertDialog.Builder(listView.getContext())
+							.setTitle(c.getString(c.getColumnIndex("title")))
+							.setMessage(msg)
+							.setPositiveButton(R.string.download,
+									new DialogInterface.OnClickListener() {
+										public void onClick(
+												DialogInterface dialog,
+												int whichButton) {
+											File d = new File(
+													android.os.Environment
+															.getExternalStorageDirectory()
+															.getAbsolutePath()
+															+ "/Android/data/com.ifrins.hipstacast/files/shows/"
+															+ podcast_id);
+											if (d.exists() == false) {
+												d.mkdirs();
+											}
 
-				 }
-				 else if (status == 0 && f.exists()) {
-					 ContentValues up = new ContentValues();
-					 up.put("status", 1);
-					 getContentResolver().update(Uri.parse("content://com.ifrins.hipstacast.provider.HipstacastContentProvider/podcasts/"+getIntent().getExtras().getString("show_id")+"/episodes/"+episode_id), 
-							 up, "_id = ?", new String[] {String.valueOf(episode_id)});
-					 Intent openIntent = new Intent(getApplicationContext(), EpisodePlayer.class);
-					 openIntent.putExtra("show_id", Integer.parseInt(getIntent().getExtras().getString("show_id")));
-					 openIntent.putExtra("episode_id", episode_id);
-					 openIntent.putExtra("type", content_type);
-					 startActivity(openIntent);
+											d = null;
+											DownloadManager mgr = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+											mgr.enqueue(new DownloadManager.Request(
+													Uri.parse(content_url))
+													.setTitle(title)
+													.setDestinationInExternalFilesDir(
+															getApplicationContext(),
+															null,
+															"shows/"
+																	+ podcast_id
+																	+ "/"
+																	+ episode_id
+																	+ ".mp3"));
 
-				 }
-				 
-				 else {
-					 Intent openIntent = new Intent(getApplicationContext(), EpisodePlayer.class);
-					 openIntent.putExtra("show_id", Integer.parseInt(getIntent().getExtras().getString("show_id")));
-					 openIntent.putExtra("episode_id", episode_id);
-					 openIntent.putExtra("type", content_type);
-					 startActivity(openIntent);
-				 }
-				 
+										}
+									})
+							.setNegativeButton(R.string.cancel,
+									new DialogInterface.OnClickListener() {
+										public void onClick(
+												DialogInterface dialog,
+												int whichButton) {
+											// Do nothing.
+										}
+									}).show();
+
+				} else if (status == 0 && f.exists()) {
+					ContentValues up = new ContentValues();
+					up.put("status", 1);
+					getContentResolver()
+							.update(Uri
+									.parse("content://com.ifrins.hipstacast.provider.HipstacastContentProvider/podcasts/"
+											+ getIntent().getExtras()
+													.getString("show_id")
+											+ "/episodes/" + episode_id), up,
+									"_id = ?",
+									new String[] { String.valueOf(episode_id) });
+					Intent openIntent = new Intent(getApplicationContext(),
+							EpisodePlayer.class);
+					openIntent.putExtra(
+							"show_id",
+							Integer.parseInt(getIntent().getExtras().getString(
+									"show_id")));
+					openIntent.putExtra("episode_id", episode_id);
+					openIntent.putExtra("type", content_type);
+					startActivity(openIntent);
+
+				}
+
+				else {
+					Intent openIntent = new Intent(getApplicationContext(),
+							EpisodePlayer.class);
+					openIntent.putExtra(
+							"show_id",
+							Integer.parseInt(getIntent().getExtras().getString(
+									"show_id")));
+					openIntent.putExtra("episode_id", episode_id);
+					openIntent.putExtra("type", content_type);
+					startActivity(openIntent);
+				}
+
 			}
 
 		});
