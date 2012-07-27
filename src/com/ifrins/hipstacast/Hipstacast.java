96c24246ca22d8fc0ca3ca09e0899fc81c10470a
Francesc <francesc.bgr+ifrins@gmail.com>
2012-07-27 16:23:11 +0200

    Added event tracker

--- src/com/ifrins/hipstacast/Hipstacast.java
+++ src/com/ifrins/hipstacast/Hipstacast.java
@@ -2,7 +2,6 @@ package com.ifrins.hipstacast;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 import android.app.Application;
-import android.content.pm.PackageManager.NameNotFoundException;
 
 public class Hipstacast extends Application {
 	public GoogleAnalyticsTracker tracker;
@@ -22,6 +21,9 @@ public class Hipstacast extends Application {
 
 	public void trackPageView(String page) {
 		tracker.trackPageView(page);
+	}
+	public void trackEvent(String event_name, String event_cat, String event_action, int event_value) {
+		tracker.trackEvent(event_cat, event_action, event_name, event_value);
 	}
 
 }
